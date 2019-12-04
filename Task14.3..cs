396c324d78cebd8d9dee7f065316fd55a13daf37
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-04 16:15:32 +0200

    Add files via upload

---
+++ Task14.3..cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace ConsoleApplication134
+{
+    class Program
+    {
+        static string[] SplitText(string[] array)
+        {
+            string text;
+            Console.WriteLine("Enter text:");
+            text = Console.ReadLine();
+            array=text.Split(' ', ',','.');
+            return array;
+        }
+        static void CreateList(List<string> list,string[] array)
+        {
+            for(int i=0;i<array.Length;i++)
+            {
+                list.Add(array[i]);
+            }
+        }
+        static void PrintList(List<string> list)
+        {
+            for(int i=0;i<list.Count;i++)
+            {
+                Console.Write("{0} ", list[i]);
+            }
+        }
+        static void ModifyList(List<string> list)
+        {
+            string word;
+            for(int i=0;i<list.Count;i++)
+            {
+                word = list[i];
+                bool result = true;
+                for(int j=i+1;j<list.Count;j++)
+                {
+                    if(word==list[j])
+                    {
+                        result = false;
+                        list.RemoveAt(j);
+                    }
+                }
+                if(result==true)
+                {
+                    Console.Write("{0} ", word);
+                }
+                
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            string[] array = null;
+            array = SplitText(array);
+            CreateList(list, array);
+            Console.WriteLine("List is:");
+            Console.WriteLine();
+            PrintList(list);
+            Console.WriteLine();
+            Console.WriteLine("Words that appear only once in the text:");
+            Console.WriteLine();
+            ModifyList(list);
+          
+           
+        }
+    }
+}
